ed51676a64b6295ea7352b97e6896062c0e49b86
David Venegas <venegasdavidm@gmail.com>
2022-08-08 15:53:57 -0400

    angular

---
+++ 2-angular/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  todos: string[] = [];
+  todoText = '';
+
+  ngOnInit() {
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos as string) || [];
+  }
+
+  addTodo() {
+    this.todos.push(this.todoText);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    
+  }
+
+}
